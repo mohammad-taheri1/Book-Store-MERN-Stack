fcdd362a201819673b62f78cfa156ca9c87fc1ce
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-05 03:23:45 +0330

    feat: add Lesson 02

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 ### Backend
 
 - [x] Lesson 01: Create Node.js project from scratch
+- [x] Lesson 02: Create our first Http Route
 
 ### Frontend
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express from 'express';
+import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+  console.log(`App is listening to port: ${PORT}`);
 });
