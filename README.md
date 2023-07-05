dd65c6500c5986a67292d0bb495c413be6f69a6a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-06 00:16:18 +0330

    feat: add Lesson 04

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [x] Lesson 01: Create Node.js project from scratch
 - [x] Lesson 02: Create our first Http Route
 - [x] Lesson 03: Add MongoDB and mongoose to node js
+- [x] Lesson 04: Create Book model with mongoose
---
+++ backend/models/bookModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model('Cat', bookSchema);
