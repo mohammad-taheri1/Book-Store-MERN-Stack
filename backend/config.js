5f4e180de5eac12306e41dad15cb23622c653f80
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-05 08:13:39 +0330

    feat: add Lesson 03

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.get('/', (request, response) => {
   return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
