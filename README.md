29bd700b9697efe2ad726202f2879dc0ab963f5e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-06 07:40:52 +0330

    feat: add Lesson 07

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [x] Lesson 04: Create Book model with mongoose
 - [x] Lesson 05: Save a new Book with mongoose
 - [x] Lesson 06: Get All Books with mongoose
+- [x] Lesson 07: Get One Book by id with mongoose
--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.post('/books', async (request, response) => {
   }
 });
 
-// Route for Get All Books frm database
+// Route for Get All Books from database
 app.get('/books', async (request, response) => {
   try {
     const books = await Book.find({});
@@ -49,6 +49,21 @@ app.get('/books', async (request, response) => {
       count: books.length,
       data: books
     });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get One Book from database by id
+app.get('/books/:id', async (request, response) => {
+  try {
+
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
