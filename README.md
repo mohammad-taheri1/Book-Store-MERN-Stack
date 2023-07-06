d5b2266a7b2e50347989652f7918b960f12afeee
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-06 03:42:49 +0330

    feat: add Lesson 05

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - [x] Lesson 02: Create our first Http Route
 - [x] Lesson 03: Add MongoDB and mongoose to node js
 - [x] Lesson 04: Create Book model with mongoose
+- [x] Lesson 05: Save a new Book with mongoose
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,44 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
+// Route for Save a new Book
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
 
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
