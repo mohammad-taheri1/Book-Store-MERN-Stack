926025aef973df2d50e1b917330b59d4b69ebb11
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-06 05:58:47 +0330

    feat: add Lesson 06

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - [x] Lesson 03: Add MongoDB and mongoose to node js
 - [x] Lesson 04: Create Book model with mongoose
 - [x] Lesson 05: Save a new Book with mongoose
+- [x] Lesson 06: Get All Books with mongoose
--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,21 @@ app.post('/books', async (request, response) => {
   }
 });
 
+// Route for Get All Books frm database
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
