bc74885a9a3acfea908c2e26b9efe42996a41e0f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-07-07 15:18:23 +0330

    feat: add Lesson 11

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,24 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (request, response) => {
   console.log(request);
